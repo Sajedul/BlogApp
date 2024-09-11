4df7f3c05199eaf9531d697c901a108d8d90aaf0
rahat <rahat14048@gmail.com>
2024-09-11 19:21:54 +0600

    third commit

--- src/main/java/com/blog/repositories/CommentRepo.java
+++ src/main/java/com/blog/repositories/CommentRepo.java
@@ -1,5 +1,9 @@
 package com.blog.repositories;
 
-public interface CommentRepo {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.blog.entities.Comment;
+
+public interface CommentRepo extends JpaRepository<Comment, Integer>{
 
 }
